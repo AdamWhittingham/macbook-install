12297539249d3a8a2e8b22d7cbadfea69bcc9e84
Adam Whittingham <adam.whittingham@gmail.com>
2014-10-18 20:17:13 +0100

    Add extra brew apps

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
+mkdir -p $HOME/projects
+
 pushd $(dirname $0) >/dev/null
-
 ln -s laptop.local ~/.laptop.local
-
 bash <(curl -s https://raw.githubusercontent.com/thoughtbot/laptop/master/mac) 2>&1 | tee ~/laptop.log
-
 popd > /dev/null
